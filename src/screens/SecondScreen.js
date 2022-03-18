a37884e1e9619a48ba38c6af2d23138a25eea271
Gayath <chandulagayan@gmail.com>
2022-03-18 21:05:27 +0530

    dashboard changes

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { View,  Image,Text } from "react-native";
 import {
   Layout,
@@ -10,14 +10,25 @@ import {
   useTheme,
 } from "react-native-rapi-ui";
 import { Ionicons,Fontisto } from "@expo/vector-icons";
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph,Divider } from 'react-native-paper';
+
+
 
 
 
 export default function ({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
+  const [MonthName,setMonthName] = useState();
+
+
+  React.useEffect(()=>{
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const d = new Date();
+    let monthName = months[d.getMonth()];
+    setMonthName(monthName);
+  })
   return (
-    <Layout>
+    <Layout >
       <TopNav
         middleContent="Menu"
         leftContent={
@@ -43,6 +54,33 @@ export default function ({ navigation }) {
           }
         }}
       />
+
+      <Text style={{ fontFamily: 'SFPRODISPLAYBOLD', fontSize:20,fontWeight: 'bold', paddingLeft:10,paddingTop:10, color: isDarkmode ? "#FFFFFF" : "#000000"}}>Good Morning, Officer</Text>
+
+      <View style={{flex:1,flexDirection: 'row', margin:15, backgroundColor: "#fff",borderRadius:10,  shadowColor: '#000',
+                  shadowOffset: { width: 0, height: 1 },
+                  shadowOpacity: 0.8,
+                  shadowRadius: 2,  
+                  elevation: 5}}>
+          {/* <Card color= {isDarkmode ? "#FFFFFF" : "#000000"} style={{flex:1,flexDirection: 'row', width: "100%",borderRadius: 10,backgroundColor: isDarkmode ? "#000000" : "#FFFFFF"}}> */}
+                <View style={{flex: 1, margin:10,alignItems: "center",justifyContent:"center"}} >
+                  <Text style={{fontFamily: 'SFPRODISPLAYBOLD',fontWeight:"bold",fontSize:80,textAlign:"center", }}>{new Date().getDate()}</Text>
+                  <Text style={{fontFamily:"SFPRODISPLAYBOLD",fontSize:20,textAlign:"center",fontWeight:"bold" }}>{MonthName}</Text>
+                </View>
+                
+                <View style={{flex: 1, margin:10,borderLeftWidth:4,borderColor:"#bcc9fe", borderRadius:2,overflow:"hidden" }} >
+                
+                  <Text style={{fontFamily: 'SFPRODISPLAYBOLD',fontSize:15,textAlign:'left',fontWeight:"bold",marginLeft:5 }}>Today Routes</Text>
+                  <Text style={{fontFamily: 'SFProTextRegular',fontSize:15,textAlign:'left',marginLeft:15,marginTop: 10}}>Udugampola - 
+                  Gampaha</Text>
+                  <Text style={{fontFamily: 'SFProTextRegular',fontSize:15,textAlign:'left',marginLeft:15,marginTop: 10}}>Udugampola - 
+                  Minuwangoda</Text>
+
+                </View>
+          {/* </Card> */}
+      </View>
+      <Text style={{ fontFamily: 'SFPRODISPLAYBOLD', fontSize:20,fontWeight: 'bold', padding:20, color: isDarkmode ? "#FFFFFF" : "#141414"}}>Let's Ensure Road Safety</Text>
+
       <View style={{flex: 1, flexDirection: 'row',alignItems: "center",justifyContent:"center"}}>
  
       
@@ -60,7 +98,7 @@ export default function ({ navigation }) {
               source={require("../../assets/driving.png")}
             /> */}
           <Fontisto name="blood-test" size={24} color= {isDarkmode ? "#FFFFFF" : "#000000"}/>
-          <Text style={{ fontFamily: 'SFPRODISPLAYBOLD', fontSize:25,fontweight: "bold", color: isDarkmode ? "#FFFFFF" : "#000000", textAlign: "center"}}>Alcohol Test</Text>
+          <Text style={{ fontFamily: 'Roboto_400Regular', fontSize:25,fontweight: "bold", color: isDarkmode ? "#FFFFFF" : "#000000", textAlign: "center"}}>Alcohol Test</Text>
          
         </SectionContent>
       </Section>
@@ -80,10 +118,10 @@ export default function ({ navigation }) {
           </Card>
         </View>
       </View>
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{flex: 1, flexDirection: 'row',margin:10}}>
  
       
-        <View style={{flex: 1, margin:10 }} >
+        <View style={{flex: 1 }} >
         <Section style={{height:200}} >
         <SectionContent>
           {/* This text using ubuntu font */}
@@ -91,7 +129,7 @@ export default function ({ navigation }) {
         </SectionContent>
       </Section>
         </View>
-        <View style={{flex: 1,margin:10}} >
+        <View style={{flex: 1}} >
         <Section style={{height:200}}>
         <SectionContent>
           {/* This text using ubuntu font */}
