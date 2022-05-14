f85c7489518a2bbb025c17290f7e038a403180fd
Gayath chandula <50480422+Gayath1@users.noreply.github.com>
2022-05-14 21:51:58 +0530

    Update nic.py

--- Backend/api/nic.py
+++ Backend/api/nic.py
@@ -3,13 +3,6 @@ from database.models import Drivers
 from flask_restful import Resource
 import json
 
-# @app.route('/api/driver/<nic>', methods=['GET'])
-# def get_driver(nic):
-#     driver = Drivers.objects(Nic=nic).first()
-#     if driver:
-#         return jsonify(driver.to_json())
-#     return jsonify({"message": "Driver not found"}), 404
-
 
 class NicApi(Resource):
     def get(self, nic):
