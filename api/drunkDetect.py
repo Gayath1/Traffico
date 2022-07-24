78f7345883ca4c7eede4604125f3adf80a69520e
Gayath chandula <50480422+gayathchandula@users.noreply.github.com>
2022-07-24 13:50:23 +0530

    Update drunkDetect.py

--- api/drunkDetect.py
+++ api/drunkDetect.py
@@ -103,10 +103,10 @@ class DrunkApi(Resource):
 
         if(DrunkVotes > SoberVotes):
             print("person is drunk")
-            return True
+            return jsonify('true')
         elif(DrunkVotes < SoberVotes):
             print("person is not drunk")
-            return False
+            return jsonify('false')
 
 
 def initialize_routes_drunkDetect(api):
